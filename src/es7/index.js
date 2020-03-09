cc010d995f0b2401ccdc050d43ba2669cf522a84
jorgelara <Jorge Lara>
2020-03-08 20:21:10 -0500

    Jorge Lara - update

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(7)) {
+    console.log("SI esta el numero 7");
+} else {
+    console.log("No!");
+}
+
+let base = 4;
+let exponent = 3;
+
+let result = base ** exponent;
+
+console.log("Result => " + result);
